03272d5e460f92f742c3bf71437ef4ef57baa55d
babarkhandev <b.khan01@hotmail.co.uk>
2024-03-13 16:50:13 +0000

    reformat commends on PlantGrowthDataResponse

--- app/Messages.py
+++ app/Messages.py
@@ -16,6 +16,8 @@ class MessageResponse(BaseModel):
 
 class PlantGrowthDataResponse(BaseModel):
     # On a given day we will see x plants, some of these will be plant_id p.
-    # We return a list where each element i, represents the proportion of the plants identified that were plant_id p
+    # Each elem in plant_growth_data represents the proportion of plants identified that were class p in a given day
+    # These will be sent to the LLM api to be formatted into a prompt for the LLM
+    # We send one of these for each plant_id we have discovered
     plant_id: str
     plant_growth_data: List[float]
