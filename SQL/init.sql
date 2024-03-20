5b862027618de6e3cdbbd1fd9ddfb6b02d606469
babarkhandev <b.khan01@hotmail.co.uk>
2024-03-20 17:51:35 +0000

    PostgreSQL supporting code
    Data.py contains code to connect to load our PostgreSQL ini file, connect to our database, insert a raw and processed entries
    
    Dockerfile updated to install the requirements for psycopg2
    
    init.sql contains to code to set up the image_processing schema, and raw/processed entry tables.

---
+++ SQL/init.sql
@@ -0,0 +1,41 @@
+-- SCHEMA: image_processing
+
+CREATE SCHEMA IF NOT EXISTS image_processing
+    AUTHORIZATION gpig;
+
+
+-- Table: image_processing.processed_entry
+
+CREATE TABLE IF NOT EXISTS image_processing.processed_entry
+(
+    id serial NOT NULL,
+    image_uri varchar COLLATE pg_catalog."default" NOT NULL,
+    plant_id varchar COLLATE pg_catalog."default",
+    CONSTRAINT processed_entry_pkey PRIMARY KEY (id),
+    CONSTRAINT processed_entry_raw_entry_image_uri_fk FOREIGN KEY (image_uri)
+        REFERENCES image_processing.raw_entry (image_uri) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS image_processing.processed_entry
+    OWNER to gpig;
+
+
+-- Table: image_processing.raw_entry
+
+CREATE TABLE IF NOT EXISTS image_processing.raw_entry
+(
+    image_uri varchar COLLATE pg_catalog."default" NOT NULL,
+    latitude double precision[] NOT NULL,
+    longitude double precision[] NOT NULL,
+    date timestamp NOT NULL,
+    CONSTRAINT raw_entry_pkey PRIMARY KEY (image_uri)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS image_processing.raw_entry
+    OWNER to gpig;
